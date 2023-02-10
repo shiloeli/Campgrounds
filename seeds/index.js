995ec1ed421146b278fd42da2f64b86221bd5f5a
shiloa3@gmail.com <shiloa3@gmail.com>
2023-02-10 14:01:18 +0200

    add img and desc

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,12 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i=0; i< 50; i++){
         const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground({
             location:`${citeis[random1000].city}, ${citeis[random1000].state}`,
-            title:`${sample(descriptors)}, ${sample(places)}`
+            title:`${sample(descriptors)}, ${sample(places)}`,
+            image:'https://unsplash.com/collections/1114848/camping',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus neque, similique sed debitis enim sapiente porro obcaecati asperiores provident aperiam natus eveniet quod consequuntur temporibus placeat distinctio iure, ipsam inventore!'
         })
         await camp.save();
     }
